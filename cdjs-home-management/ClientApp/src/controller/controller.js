a312425afbd99d098c2653ebf33b8b81711caa87
Dilesh <drj855@mun.ca>
2019-11-06 00:12:46 -0330

    changing repo

---
+++ cdjs-home-management/ClientApp/src/controller/controller.js
@@ -0,0 +1,7 @@
+(function () {'use strict';angular.module('app').controller('controller', controller);controller.$inject = ['$scope'];
+    function controller($scope) {
+        $scope.title = 'controller';
+        activate();
+        function activate() { }
+    }
+})();
