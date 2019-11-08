1402334d2222d758a7932f0bfaddff28f6c5799a
Dilesh <drj855@mun.ca>
2019-11-07 21:59:56 -0330

    data binding

--- cdjs-home-management/ClientApp/src/app/login/login.component.ts
+++ cdjs-home-management/ClientApp/src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RequestServices } from "../services/requestServices";
-import {Credential} from "../models/credential"
-import { fakeAsync } from '@angular/core/testing';
+import { Credential } from "../models/credential";
 
 @Component({
     selector: 'app-login',
@@ -10,13 +9,13 @@ import { fakeAsync } from '@angular/core/testing';
 })
 //const _reqServices = new RequestServices();
 export class LoginComponent {
+    loginModel: Credential;
     constructor(
         private _serviceRequest: RequestServices
-    ) { }
-    getPermission() {
-        let fakedata: Credential = new Credential();
-        fakedata.UserName = "test";
-        fakedata.PassWord = "test";
-      this._serviceRequest.checkCredential(fakedata);
-  }
+    ) {
+        this.loginModel =  new Credential();
+    }
+    loginUser() {
+        this._serviceRequest.checkCredential(this.loginModel);
+    }
 }
