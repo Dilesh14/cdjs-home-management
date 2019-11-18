fc241cea4cdb4fff9f5fb62f913023990a9bb0ae
Dilesh <drj855@mun.ca>
2019-11-18 20:04:18 -0330

    building repo [upto repo base done].... need to implement all the interface function and make wrapper

---
+++ cdjs-home-management/Repository/Abstract/RepositoryBase.cs
@@ -0,0 +1,44 @@
+﻿using cdjs_home_management.DbContexts;
+using cdjs_home_management.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace cdjs_home_management.Repository.Abstract
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected cdjsentity _repoContext { get; set; }
+
+        public RepositoryBase(cdjsentity context) 
+        {
+            this._repoContext = context;
+        }
+        public void Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ cdjs-home-management/Repository/RespositoryWrapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cdjs-home-management.Repository
+{
+    public class RespositoryWrapper
+{
+}
+}
