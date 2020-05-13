4d8eea196ac85d57ef3b5ead9c2e8bb8f259cdab
dilesh <drj855@mun.ca>
2020-05-12 23:05:13 -0230

    Repo created

---
+++ cdjs-home-management/Repository/Interface/ITaskRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using cdjs.entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace cdjs_home_management.Repository.Interface
+{
+    public interface ITaskRepository
+    {
+        Task<ICollection<cdjs.entities.task>> GetAll();
+        Task<bool> Add(task newTask);
+        Task<bool> Delete(task task);
+        Task<bool> Update(task task);
+    }
+}
---
+++ cdjs-home-management/Repository/TaskRepository.cs
@@ -0,0 +1,37 @@
+﻿using cdjs.entities;
+using cdjs_home_management.DbContexts;
+using cdjs_home_management.Repository.Abstract;
+using cdjs_home_management.Repository.Interface;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace cdjs_home_management.Repository
+{
+    public class TaskRepository :RepositoryBase<task>, ITaskRepository
+    {
+        private readonly cdjsentity _context;
+        public TaskRepository(cdjsentity context): base(context) 
+        {
+            _context = context;
+        }
+        public Task<bool> Add(task newTask)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> Delete(task task)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ICollection<task>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> Update(task task)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
